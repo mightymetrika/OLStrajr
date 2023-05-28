d69a5b697dc5982e77b890fc7f0849460fe54dd0
Mackson Ncube <macksonncube@gmail.com>
2023-05-28 15:23:53 +0000

    add mean and outlier labels to boxplot.

--- tests/testthat/test-OLStraj.R
+++ tests/testthat/test-OLStraj.R
@@ -3,11 +3,26 @@
 #                    var1 = c(3,7,4,5,8),
 #                    var2 = c(7,3,9,4,7),
 #                    var3 = c(8,5,3,9,7),
-#                    var4 = c(1,5,3,9,6))
+#                    var4 = c(1,5,3,9,30))
 #
 #   olstraj_out <- OLStraj(data = df,
 #                          varlist = c("var1", "var2", "var3", "var4"),
 #                          regtype = "both")
+#
+#   # Debugging parameters:
+#   data = df
+#   varlist = c("var1", "var2", "var3", "var4")
+#   regtype = "both"
+#   idvarname = "id"
+#   predvarname = "time"
+#   outvarname = "score"
+#   timepts = c(0, 1, 2, 3)
+#   inclmiss = "n"
+#   level = "both"
+#   numplot = NULL
+#   hist = "y"
+#   box = "y"
+#   outds = TRUE
 #
 #   expect_equal(2 * 2, 4)
 # })
