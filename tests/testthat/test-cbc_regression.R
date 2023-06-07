499d362129e9ed25fa166b693c881db4d731084f
Mackson Ncube <macksonncube@gmail.com>
2023-06-07 15:42:54 +0000

    generalize cbc_lm to arbitrary lm formula

--- tests/testthat/test-cbc_regression.R
+++ tests/testthat/test-cbc_regression.R
@@ -1,9 +1,10 @@
-test_that("cbc_lm works", {
-
-  df <- data.frame(ids = rep(1:5, 5),
-                   vals = stats::rnorm(25),
-                   outs = stats::rnorm(25, 10, 5))
-
-  res <- cbc_lm(data = df, formula = "outs ~ vals", .case = "ids")
-  expect_equal(2 * 2, 4)
-})
+# test_that("cbc_lm works", {
+#
+#   df <- data.frame(ids = rep(1:5, 20),
+#                    vals = stats::rnorm(100),
+#                    outs = stats::rnorm(25, 10, 100))
+#
+#   res <- cbc_lm(data = df, formula = "outs ~ vals", .case = "ids")
+#   expect_equal(2 * 2, 4)
+# })
+#
